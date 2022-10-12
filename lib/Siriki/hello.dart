6d607a761d340e8ea46f474d792ca00c1d56800e
SirikiditNaleleFofana <Siriki.fofana@uvci.edu.ci>
2022-10-12 16:22:19 +0000

    lll

--- lib/Siriki/hello.dart
+++ lib/Siriki/hello.dart
@@ -5,6 +5,8 @@ class name extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text("Hello moi je teste"),
+    );
   }
 }
